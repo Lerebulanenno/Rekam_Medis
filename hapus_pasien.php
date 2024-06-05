6457c17673ea7a81d74cc90bd34b972d01ce9f56
Enno Lerebulan <148016035+Lerebulanenno@users.noreply.github.com>
2024-06-05 10:02:27 +0800

    Menambahkan kode hapus pasien

---
+++ hapus_pasien.php
@@ -0,0 +1,38 @@
+<?php
+// Include file konfigurasi untuk koneksi ke database
+include 'config.php';
+
+// Mendapatkan ID pasien dari parameter URL
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+
+    // Query untuk menghapus data pasien berdasarkan ID
+    $sql_delete = "DELETE FROM patients WHERE id = ?";
+
+    if ($stmt = $conn->prepare($sql_delete)) {
+        // Bind parameter ke statement
+        $stmt->bind_param("i", $id);
+
+        // Eksekusi statement
+        if ($stmt->execute()) {
+            // Redirect ke halaman tampil_pasien.php setelah berhasil menghapus data
+            header("location: tampil_pasien.php");
+            exit();
+        } else {
+            echo "Terjadi kesalahan. Silakan coba lagi nanti.";
+        }
+
+        // Tutup statement
+        $stmt->close();
+    } else {
+        echo "Terjadi kesalahan. Silakan coba lagi nanti.";
+    }
+} else {
+    // Jika ID tidak ditemukan di URL, redirect ke halaman tampil_pasien.php
+    header("location: tampil_pasien.php");
+    exit();
+}
+
+// Tutup koneksi database
+$conn->close();
+?>
